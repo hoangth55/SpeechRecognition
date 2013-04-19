825c04a20a339a0a9c3e0f67ac1a59c8418f3cf4
hoangth55 <hoangth_55@vnu.edu.vn>
2013-04-19 14:46:58 +0700

    Artificial Neural Network
    Using Back-propagation algorithm to construct ANN. This ANN with the
    layer-nerons per layer was configed in config.nn.

---
+++ ANN/trace.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+class CTRACE
+{
+
+        FILE *fp;   //file handle
+
+public:
+        int m_status;
+
+        CTRACE(wchar_t *name);
+        ~CTRACE();
+
+        void dump(float f);
+        void dump(int d);
+        void flush(void);
+};
+
